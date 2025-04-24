9fa6aba5e611a019ba262a6edb35e28130d68d99
konbraphat51 <konbraphat@gmail.com>
2025-04-24 17:53:34 +0900

    components

---
+++ src/components/CliBox.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name: "CliBox",
+	props: {
+		content: String,
+	},
+}
+</script>
+
+<template>
+	<div class="CliBox">
+		<textarea :placeholder="content"></textarea>
+	</div>
+</template>
+
+<style scoped></style>
