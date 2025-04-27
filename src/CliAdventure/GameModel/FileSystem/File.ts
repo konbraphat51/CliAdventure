c63fc72a6d738b4ae0fcefbdcee6474d1c5c8075
konbraphat51 <konbraphat@gmail.com>
2025-04-27 16:20:10 +0900

    refactor: simplify File and FileSystemEntry classes by removing redundant properties and constructors

--- src/CliAdventure/GameModel/FileSystem/File.ts
+++ src/CliAdventure/GameModel/FileSystem/File.ts
@@ -3,21 +3,15 @@ import {FileSystemEntry} from "./FileSystemEntry"
 import {FileType} from "./FileType"
 
 export abstract class File extends FileSystemEntry {
-	name: string
-	parent: FileSystemEntry | null
 	fileType: FileType
-	entryType: FileSystemEntryType
 
 	constructor(
 		name: string,
 		parent: FileSystemEntry | null,
 		fileType: FileType,
 	) {
-		super()
-		this.name = name
-		this.parent = parent
+		super(name, parent, FileSystemEntryType.File)
 		this.fileType = fileType
-		this.entryType = FileSystemEntryType.File
 	}
 
 	getFullPath(): string {
--- src/CliAdventure/GameModel/FileSystem/FileSystemEntry.ts
+++ src/CliAdventure/GameModel/FileSystem/FileSystemEntry.ts
@@ -1,11 +1,31 @@
 import {FileSystemEntryType} from "./FileSystemEntryType"
 
 export abstract class FileSystemEntry {
-	abstract get name(): string
-	abstract get parent(): FileSystemEntry | null
-	abstract get entryType(): FileSystemEntryType
+	get name(): string {
+		return this._name
+	}
+	get parent(): FileSystemEntry | null {
+		return this._parent
+	}
+	get entryType(): FileSystemEntryType {
+		return this._entryType
+	}
 
-	getFullPath(): string {
+	protected _name: string
+	protected _parent: FileSystemEntry | null
+	protected _entryType: FileSystemEntryType
+
+	constructor(
+		name: string,
+		parent: FileSystemEntry | null,
+		entryType: FileSystemEntryType,
+	) {
+		this._name = name
+		this._parent = parent
+		this._entryType = entryType
+	}
+
+	public getFullPath(): string {
 		return `${this.parent ? this.parent.getFullPath() + "/" : ""}${this.name}`
 	}
 }
